64ab11037587f861e241e79e31d5a20ea49b0bf5
Vanessa Toro <vanetoro84@gmail.com>
2018-07-02 14:57:39 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,9 +26,9 @@ class PostsController < ApplicationController
 	def update
 		@post = Post.find(params[:id])
 		@post.title = params[:title]
-	 @post.description = params[:description]
-	 @post.save
-	 redirect_to post_path(@post)
+	 	@post.description = params[:description]
+	 	@post.save
+	 	redirect_to post_path(@post)
 	end
 
 
